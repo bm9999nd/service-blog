b664692713cc5d6d675ced2219d8466e12140f1d
bm9999nd <bm9999nd@users.noreply.github.com>
2024-03-02 20:49:52 +0700

    Add files via upload

--- scanner/ctl-scan.js
+++ scanner/ctl-scan.js
@@ -1,189 +1,202 @@
 // --- modal
-const evSelect = "camSelect";
-const modal = new bootstrap.Modal("#modal-cam", {
-  backdrop: true,
-  focus: true,
-  keyboard: true,
-});
+const modal = {
+  eventName: "camSelect",
+  getModal: new bootstrap.Modal("#modal-cam", {
+    backdrop: true,
+    focus: true,
+    keyboard: true,
+  }),
+  
+  renderModal: function (items = []) {
+    const mod = $("#modal-cam");
+    const ul = mod.find(".cam-select");
+    ul.children().remove();
+  
+    items.forEach((item) => {
+      const li = $($("#tmp-radio").clone().html());
+      const id = item.id;
+      li.find("label").attr("for", id).html(item.label);
+      li.find("input")
+        .attr("id", id)
+        .val(id)
+        .on("input", (e) => {
+          const btn = mod.find(".btn-primary");
+          btn.prop("disabled", false);
+        });
+      li.appendTo(ul);
+    });
+  
+    modal.getModal.show();
+  },
+  
+  init: () => {
+    $("#modal-cam .btn-primary").on("click", (e) => {
+      const sel = $("#modal-cam [name='camera']:checked").val();
 
-function renderModal(items = []) {
-  const mod = $("#modal-cam");
-  const ul = mod.find(".cam-select");
-  ul.children().remove();
+      if (sel != null) {
+        document.dispatchEvent(
+          new CustomEvent(modal.eventName, {
+            detail: sel,
+          })
+        );
+        modal.getModal.hide();
 
-  items.forEach((item) => {
-    const li = $($("#tmp-radio").clone().html());
-    const id = item.id;
-    li.find("label").attr("for", id).html(item.label);
-    li.find("input")
-      .attr("id", id)
-      .val(id)
-      .on("input", (e) => {
-        const btn = mod.find(".btn-primary");
-        btn.prop("disabled", false);
-      });
-    li.appendTo(ul);
-  });
-
-  modal.show();
-}
-
-$("#modal-cam .btn-primary").on("click", (e) => {
-  const sel = $("#modal-cam [name='camera']:checked").val();
-  if (sel != null) {
-    document.dispatchEvent(
-      new CustomEvent(evSelect, {
-        detail: sel,
-      })
-    );
-    modal.hide();
-  } else {
-    alert("Camera not selected!");
-  }
-});
+      } else {
+        alert("Camera not selected!");
+      }
+    });
+  },
+};
 
 // --- scanner
-const eventScanName = "scanState";
-let lastScanned = "";
-let scanner;
-
-async function getCamera() {
-  try {
-    const exist = await Html5Qrcode.getCameras();
-    if (!exist || exist.length == 0) {
-      return console.error("no camera found!");
-    }
-
-    console.log("Cameras:\n", exist);
-    renderModal(exist);
-  } catch (error) {
-    alert(error.message);
-  }
-}
-
-function scanning(
-  camId,
-  videoId,
-  resultId,
-  eventName = "scanned",
-  singleScan = false
-) {
-  const html5QrCode = new Html5Qrcode(videoId, false);
-  const result = $(`#${resultId}`);
-
-  function start() {
+const scanner = {
+  eventScannedName: "scanned",
+  eventStateName: "scanState",
+  lastScanned: "",
+  scanner: undefined,
+  
+  getCamera: async function () {
     try {
-      document.dispatchEvent(
-        new CustomEvent(eventScanName, {
-          detail: true,
-        })
-      );
+      const exist = await Html5Qrcode.getCameras();
+      if (!exist || exist.length == 0) {
+        return console.error("no camera found!");
+      }
+  
+      console.log("Cameras:\n", exist);
+      modal.renderModal(exist);
 
-      html5QrCode.start(
-        camId,
-        {
-          fps: 10, // Optional, frame per seconds for qr code scanning
-          qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
-        },
-        (decodedText, decodedResult) => {
-          // do something when code is ready
-          if (decodedText == lastScanned) return;
-
-          result[0].dispatchEvent(
-            new CustomEvent(eventName, {
-              detail: {
-                text: decodedText,
-                result: decodedResult,
-              },
-            })
-          );
-          lastScanned = decodedText;
-
-          if (singleScan) stop();
-        },
-        (errorMessage) => {
-          // parse error, ignore it.
-        }
-      );
-    } catch (err) {
-      // Start failed, handle it.
-      console.warn("failed to start camera:\n", err);
-      document.dispatchEvent(
-        new CustomEvent(eventScanName, {
-          detail: false,
-        })
-      );
-      alert(err.message);
+    } catch (error) {
+      alert(error.message);
     }
-  }
-
-  async function stop() {
-    try {
-      const stopped = await html5QrCode.stop();
-      // QR Code scanning is stopped.
-      document.dispatchEvent(
-        new CustomEvent(eventScanName, {
-          detail: false,
-        })
-      );
-    } catch (err) {
-      // Stop failed, handle it.
+  },
+  
+  scanning: function (camId, videoId, resultId, singleScan = false) {
+    const qrCode = new Html5Qrcode(videoId, false);
+    const result = $(`#${resultId}`);
+  
+    function start() {
+      try {
+        document.dispatchEvent(
+          new CustomEvent(scanner.eventStateName, {
+            detail: true,
+          })
+        );
+  
+        qrCode.start(
+          camId,
+          {
+            fps: 10, // Optional, frame per seconds for qr code scanning
+            qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+          },
+          (decodedText, decodedResult) => {
+            // do something when code is ready
+            if (decodedText == scanner.lastScanned) return;
+  
+            result[0].dispatchEvent(
+              new CustomEvent(scanner.eventScannedName, {
+                detail: {
+                  text: decodedText,
+                  result: decodedResult,
+                },
+              })
+            );
+            scanner.lastScanned = decodedText;
+  
+            if (singleScan) stop();
+          },
+          (errorMessage) => {
+            // parse error, ignore it.
+          }
+        );
+      } catch (err) {
+        // Start failed, handle it.
+        console.warn("failed to start camera:\n", err);
+        document.dispatchEvent(
+          new CustomEvent(scanner.eventStateName, {
+            detail: false,
+          })
+        );
+        alert(err.message);
+      }
     }
-  }
+  
+    async function stop() {
+      try {
+        await qrCode.stop();
 
-  return {
-    start,
-    stop,
-    scanner: html5QrCode,
-  };
-}
-
-$("#result").on("scanned", (e) => {
-  console.info(
-    "decoded text:\n",
-    e.detail.text,
-    "decoded result:\n",
-    e.detail.result
-  );
-  $("#audio")[0].play();
-  const c = $(e.target);
-  c.html(e.detail.text);
-});
-
-$(document).on(eventScanName, (e) => {
-  const isPlay = e.detail;
-  const btn = $("button.player");
-
-  if (!isPlay) scanner = undefined;
-  setCss(isPlay);
-
-  function setCss(isPlaying) {
-    const icon = btn.find("i");
-    const img = {
-      playBtn: "btn-primary",
-      stopBtn: "btn-danger",
-      playIcon: "bi-qr-code-scan",
-      stopIcon: "bi-stop-fill",
+        // QR Code scanning is stopped.
+        document.dispatchEvent(
+          new CustomEvent(scanner.eventStateName, {
+            detail: false,
+          })
+        );
+      } catch (err) {
+        // Stop failed, handle it.
+      }
+    }
+  
+    return {
+      start,
+      stop,
+      scanner: qrCode,
     };
+  },
+  
+  init: function () {
+    $("#result").on(scanner.eventScannedName, (e) => {
+      console.info(
+        "decoded text:\n",
+        e.detail.text,
+        "decoded result:\n",
+        e.detail.result
+      );
+      $("#audio")[0].play();
 
-    btn.toggleClass(img.playBtn, !isPlaying);
-    icon.toggleClass(img.playIcon, !isPlaying);
-    btn.toggleClass(img.stopBtn, isPlaying);
-    icon.toggleClass(img.stopIcon, isPlaying);
-  }
-});
+      const c = $(e.target);
+      c.html(e.detail.text);
+    });
+    
+    $(document).on(scanner.eventStateName, (e) => {
+      const isPlay = e.detail;
+      const btn = $("button.player");
+    
+      if (!isPlay) scanner.scanner = undefined;
+      setCss(isPlay);
+    
+      function setCss(isPlaying) {
+        const icon = btn.find("i");
+        const img = {
+          playBtn: "btn-primary",
+          stopBtn: "btn-danger",
+          playIcon: "bi-qr-code-scan",
+          stopIcon: "bi-stop-fill",
+        };
+    
+        btn.toggleClass(img.playBtn, !isPlaying);
+        icon.toggleClass(img.playIcon, !isPlaying);
+        btn.toggleClass(img.stopBtn, isPlaying);
+        icon.toggleClass(img.stopIcon, isPlaying);
+      }
+    });
+    
+    $(document).on(modal.eventName, (e) => {
+      const camId = e.detail;
+      scanner.scanner = scanner.scanning(camId, "reader", "result");
+    
+      console.log("Status:", scanner.scanner);
+      scanner.scanner.start();
+    });
+    
+    $("button.player").on("click", (e) => {
+      if (typeof scanner.scanner != "undefined") {
+        scanner.scanner.stop();
+        scanner.lastScanned = "";
+      } else scanner.getCamera();
+    });
+  },
+};
 
-$(document).on(evSelect, (e) => {
-  const camId = e.detail;
-  scanner = scanning(camId, "reader", "result");
-
-  console.log("Status:", scanner.scanner);
-  scanner.start();
-});
-
-$("button.player").on("click", (e) => {
-  if (typeof scanner != "undefined") {
-    scanner.stop();
-    lastScanned = "";
-  } else getCamera();
+$.getScript("https://unpkg.com/html5-qrcode").done((res) => {
+  modal.init();
+  scanner.init();
 });
